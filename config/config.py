7df599a56757ed9b05297d6eb76b654504666285
emingit4 <coniuk7@gmail.com>
2025-01-25 22:29:29 +0400

    Update config.py
    Signed-off-by: emingit4 <coniuk7@gmail.com>

--- config/config.py
+++ config/config.py
@@ -80,7 +80,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/emingit4/YukkiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
